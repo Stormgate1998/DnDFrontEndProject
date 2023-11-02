01477ddbcddf2a3083afab5c0beda0aa1f6035ed
Joshua Barlow <jalapenojosh@gmail.com>
2023-11-02 14:06:33 -0600

    removed linter error, maybe made another one

--- dndfrontend/src/App.tsx
+++ dndfrontend/src/App.tsx
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [testnotused, setTestNotUsed] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
