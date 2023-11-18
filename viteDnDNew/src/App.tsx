27f4db51dcbee4ddebfe740a0506f836bb22d973
Joshua Barlow <jalapenojosh@gmail.com>
2023-11-18 11:00:16 -0700

    radnom changes

--- viteDnDNew/src/App.tsx
+++ viteDnDNew/src/App.tsx
@@ -1,8 +1,10 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useAuth } from "react-oidc-context";
 import { CharacterMaker } from "./pages/CharacterMaker";
 import { CharacterViewer } from "./pages/CharacterViewer";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UserCharacterList } from "./pages/UserCharacterList";
+
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
@@ -16,15 +18,47 @@ const router = createBrowserRouter([
   },
   {
     path: "/character/:characterId",
-    element: <CharacterViewer/>,
+    element: <CharacterViewer />,
   },
 ]);
+
 export const App = () => {
+  const auth = useAuth();
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          {/* Your other components go here */}
+          <RouterProvider router={router} />
+          Hello {auth.user?.profile.sub}{" "}
+          <button onClick={() => void auth.removeUser()}>Log out</button>
+        </div>
+      </QueryClientProvider>
+    );
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        {/* <Navbar/> */}
+        {/* Your other components go here */}
         <RouterProvider router={router} />
+        <button onClick={() => void auth.signinRedirect()}>Log in</button>
       </div>
     </QueryClientProvider>
   );
--- viteDnDNew/src/main.tsx
+++ viteDnDNew/src/main.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap";
-import "./custom.scss"
+import "./custom.scss";
+import { AuthProvider } from "react-oidc-context";
 import { App } from "./App";
+
+const oidcConfig = {
+  authority: "<your authority>",
+  client_id: "<your client id>",
+  redirect_uri: "<your redirect uri>",
+  // ...
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider {...oidcConfig}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthProvider>
 );
