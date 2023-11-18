4b7b6f98f0be91fe8d762eade0f66184b83e6a20
Joshua Barlow <jalapenojosh@gmail.com>
2023-11-18 12:11:19 -0700

    radnom changes

--- viteDnDNew/src/main.tsx
+++ viteDnDNew/src/main.tsx
@@ -7,9 +7,9 @@ import { AuthProvider } from "react-oidc-context";
 import { App } from "./App";
 
 const oidcConfig = {
-  authority: "http://joshbarlowkeycloak:8080",
+  authority: "http://joshbarlowkeycloak:8080/auth", // replace with your Keycloak base URL
   client_id: "DND User",
-  redirect_uri: "https://dndbarlowproject.duckdns.org:2320/",
+  redirect_uri: "https://dndbarlowproject.duckdns.org:2320/", // replace with your redirect URI
   // ...
 };
 
